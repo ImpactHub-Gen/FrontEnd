5f808383973d8dd0d17d920b8308fde27af2af6a
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2024-02-05 16:38:11 -0300

    estilo navbar corrigido

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -19,17 +19,18 @@ function Navbar() {
 
     if(usuario.token !== "") {
       navbarComponent = (
-        <div className='w-full flex justify-center p-4 bg-gradient-to-r from-orange-400 to-blue-hl py-12'>
+        <div className='w-full flex justify-center p-4 bg-gradient-to-r from-orange-400 to-blue-hl py-8'>
           <div className='container flex justify-between text-lg'>
             <Link to='/home' className='text-2xl font-bold'>ImpactHub</Link>
             <div>
                 <input className='w-96 border-2 border-black' type="search" name="" id="" />
             </div>
             <div className='flex gap-4 text-white font-semibold'>
-                <Link to='/sobre' className='hover:text-orange-hl'>Sobre nós</Link>
-                <Link to='/temas' className='hover:text-orange-hl'>Temas</Link>
-                <Link to='/cadastroTema' className='hover:text-orange-hl'>Cadastrar tema</Link>
-                <Link to='' onClick={logout} className='hover:text-orange-hl'>Sair</Link>
+              <Link to='/home' className='hover:text-orange-hl'>Home</Link>
+              <Link to='/temas' className='hover:text-orange-hl'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:text-orange-hl'>Cadastrar tema</Link>
+              <Link to='/sobre' className='hover:text-orange-hl'>Sobre nós</Link>
+              <Link to='' onClick={logout} className='hover:text-orange-hl'>Sair</Link>
             </div>
           </div>
         </div>
