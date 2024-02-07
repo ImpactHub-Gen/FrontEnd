dfee1b9a5b33a4698556d4e148ce1901ba4f825b
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-02-07 00:35:14 -0300

    atualização de cores do card

--- src/components/postagens/deletarPostagem/deletarPostagem.tsx
+++ src/components/postagens/deletarPostagem/deletarPostagem.tsx
@@ -70,7 +70,7 @@ function DeletarPostagem() {
             <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar a postagem a seguir?</p>
 
             <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-                <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Postagem</header>
+                <header className='py-2 px-6 bg-orange-hl text-white font-bold text-2xl'>Postagem</header>
                 <div className='p-4'>
                     <p className='text-xl h-full'>{postagem.titulo}</p>
                     <p>{postagem.texto}</p>
@@ -78,8 +78,8 @@ function DeletarPostagem() {
                     <p>{postagem.imagem}</p>
                 </div>
                 <div className='flex'>
-                    <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarPostagem}>Sim</button>
-                    <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
+                    <button className='w-full text-white bg-orange-hl hover:bg-orange-normal flex items-center justify-center' onClick={deletarPostagem}>Sim</button>
+                    <button className='text-white bg-blue-normal hover:bg-blue-hl w-full py-2' onClick={retornar}>Não</button>
                 </div>
             </div>
         </div>
